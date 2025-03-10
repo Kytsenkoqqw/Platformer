7aa458edc7e8823bff404f6a2f1cbde80ba39043
Kytsenkoqqw <136856664+Kytsenkoqqw@users.noreply.github.com>
2025-03-10 13:27:14 +0200

    add sunshine effect

---
+++ Platformer/Assets/Scripts/Light/LightFollow.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+namespace Light
+{
+    public class LightFollow : MonoBehaviour
+    {
+        [SerializeField] private Transform _target;
+
+        private void LateUpdate()
+        {
+            transform.position = _target.position;
+        }
+    }
+}
